3d590e3d07b067d663c5c4d1998771c669837a4f
Sumit Nalavade <vs.nalavade2003@gmail.com>
2024-09-05 08:03:13 -0500

    Add player cards

--- src/Game.tsx
+++ src/Game.tsx
@@ -2,6 +2,23 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { v4 as uuid } from "uuid";
 
+interface UnoCard {
+    number: number;
+    color: string;
+}
+
+const colors = ["red", "blue", "green", "yellow"];
+const numbers = Array.from({ length: 10 }, (_, index) => index); // 0 through 9
+
+const generateRandomCard = (): UnoCard => {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+    return { number: randomNumber, color: randomColor };
+};
+
+const generateRandomCards = (count: number): UnoCard[] => {
+    return Array.from({ length: count }, generateRandomCard);
+};
 
 
 const Game: React.FC = () => {
@@ -9,14 +26,17 @@ const Game: React.FC = () => {
     const [playerId, setPlayerId] = useState(uuid());
     const [broadcastChannel, setBroadcastChannel] = useState(new BroadcastChannel(gameId!));
     
+    const [cards, setCards] = useState<UnoCard[]>([]);
 
     useEffect(() => {
         if (!gameId) return;
 
         broadcastChannel.postMessage({ type: "PLAYER_JOIN", playerId });
 
+        setCards(generateRandomCards(7));
     }, [])
     
+    console.log(cards);
 
     return (
         <h1>Hello Game!</h1>
